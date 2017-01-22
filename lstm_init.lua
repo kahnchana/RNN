54fed51deb6ea4c1e71dfb2a04e7eb5e4699bb14
Kanchana Ranasinghe <kahnchana@gmail.com>
2017-01-22 19:10:06 +0530

    Update lstm_init.lua

--- lstm_init.lua
+++ lstm_init.lua
@@ -16,20 +16,20 @@ function lstm_init(kwargs)
   local inputDim = 1000 --pre specified input dimension
   local lstm = {}
   
-  lstm.numHidden = lstmHidden -- 256 default
-
+  lstm.numHidden = lstmHidden -- 30 default
+  
   -- Reshape for LSTM; N items x T sequence length x H hidden size
   model:add(nn.LSTM(inputDim, lstm.numHidden))
-
+  model:add(nn.Transpose(dim2, dim3))
+  model:add(nn.View(-1, seqLength))
   if dropout > 0 then
     model:add(nn.Dropout(dropout))
   end
+  model:add(nn.Linear(seqLength,1))
   model:add(nn.View(-1, lstm.numHidden))
-  model:add(nn.Linear(lstm.numHidden, numClasses))
-  model:add(nn.View(1072,59))
-  model:add(nn.Linear(seqLength, numClasses))
-  model:add(nn.View(1072))
-  model:add(nn.LogSoftMax())
+  model:add(nn.Linear(lstm.numHidden, numClasses))  
+--  model:add(nn.LogSoftMax())
+
 
   return model
 end
